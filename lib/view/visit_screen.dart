b7e1deaa598611cddfcbb70f057d179b29d6ffa8
Garvita <garvitasingh>
2024-01-09 14:32:59 +0530

    Initial commit

---
+++ lib/view/visit_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class VisitScreen extends StatelessWidget {
+  const VisitScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
