39abb743bbfe99a66e47580c27f6cfdcecbce8bc
Garvita <garvitasingh>
2024-01-09 14:43:51 +0530

    color

---
+++ lib/utils/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xFF323560);
+const Color secondaryColor = Color(0xFFECEDF8);
+const Color darkColor = Color(0xFF070615);
+const Color gradientColor = Color(0xFF0F1025);
+const Color backgroundColor = Color(0xFFEAEAEB);
--- lib/view/visit_screen.dart
+++ lib/view/visit_screen.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:workmate_01/utils/colors.dart';
 
 class VisitScreen extends StatelessWidget {
   const VisitScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: backgroundColor,
+        title: Text("Visit"),
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.notifications_rounded))
+        ],
+      ),
+    );
   }
 }
