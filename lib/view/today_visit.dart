5bc8386d322656b26ee9f92bd3d66b9f0250eb37
Garvita <garvitasingh>
2024-01-10 17:59:57 +0530

    today_visit.dart

--- lib/view/today_visit.dart
+++ lib/view/today_visit.dart
@@ -1,14 +1,11 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:intl/intl.dart';
-import 'package:workmate_01/component/check_in_button.dart';
 import 'package:workmate_01/utils/colors.dart';
 import 'package:geocoding/geocoding.dart';
-import 'package:image_picker/image_picker.dart';
 
 class TodayVisit extends StatefulWidget {
+
   const TodayVisit({super.key});
 
   @override
@@ -19,7 +16,6 @@ class _TodayVisitState extends State<TodayVisit> {
   String selectedLocation = 'Pune-Mumbai';
   late Position _currentPosition;
   String Address = 'Location';
-  File? _capturedImage;
 
   @override
   void initState() {
@@ -32,6 +28,7 @@ class _TodayVisitState extends State<TodayVisit> {
       _currentPosition = await _getGeoLocationPosition();
       await GetAddressFromLatLong(_currentPosition);
     } catch (e) {
+      // Handle any exceptions or errors here
       print('Error initializing location: $e');
     }
   }
@@ -69,24 +66,12 @@ class _TodayVisitState extends State<TodayVisit> {
 
   Future<void> GetAddressFromLatLong(Position position) async {
     List<Placemark> placemarks =
-        await placemarkFromCoordinates(position.latitude, position.longitude);
+    await placemarkFromCoordinates(position.latitude, position.longitude);
     print(placemarks);
     Placemark place = placemarks[0];
     Address =
-        '${place.street}, ${place.subLocality}, ${place.locality}, ${place.postalCode}, ${place.country}';
+    '${place.street}, ${place.subLocality}, ${place.locality}, ${place.postalCode}, ${place.country}';
     setState(() {});
-  }
-
-  Future<void> _openCamera() async {
-    final imagePicker = ImagePicker();
-    final pickedFile = await imagePicker.pickImage(source: ImageSource.camera);
-
-    if (pickedFile != null) {
-      setState(() {
-        _capturedImage = File(pickedFile.path);
-        _initializeLocation();
-      });
-    }
   }
 
   @override
@@ -99,14 +84,14 @@ class _TodayVisitState extends State<TodayVisit> {
       backgroundColor: backgroundColor,
       appBar: AppBar(
         backgroundColor: appbarColor,
-        title: Text(
-          "Today Visit",
-        ),
+        title: Text("Today Visit",),
         actions: [
           Container(
             padding: const EdgeInsets.symmetric(horizontal: 8.0),
             margin: const EdgeInsets.all(8.0),
-            decoration: BoxDecoration(color: Colors.white30),
+            decoration: BoxDecoration(
+              color: Colors.white30
+            ),
             child: DropdownButton<String>(
               value: selectedLocation,
               onChanged: (String? newValue) {
@@ -114,14 +99,14 @@ class _TodayVisitState extends State<TodayVisit> {
                   selectedLocation = newValue!;
                 });
               },
-              elevation: 2,
-              items: <String>['Pune-Mumbai', 'Noida-Delhi']
+              elevation: 2, // Control the shadow appearance
+              items: <String>['Pune-Mumbai','Noida-Delhi']
                   .map<DropdownMenuItem<String>>((String value) {
                 return DropdownMenuItem<String>(
                   value: value,
                   child: Text(
                     value,
-                    style: TextStyle(color: Colors.black),
+                    style: TextStyle(color: Colors.black), // Text color of dropdown items
                   ),
                 );
               }).toList(),
@@ -136,98 +121,62 @@ class _TodayVisitState extends State<TodayVisit> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Center(
-                child: Text(
-                  formattedTime,
-                  style: TextStyle(
-                      color: primaryColor,
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold),
-                ),
+                child: Text(formattedTime,
+                style: TextStyle(
+                  color: primaryColor,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold
+                ),),
               ),
-              Center(child: Text(formattedDate)),
-              Container(
-                alignment: Alignment.center,
-                height: 200,
-                child: CheckInButton(checkIn: true),
-              ),
-              _currentPosition == null
-                  ? Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              Center(
+                  child: Text(formattedDate)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  RichText(
+                    text:TextSpan(
                       children: [
-                        RichText(
-                          text: TextSpan(children: [
-                            WidgetSpan(
-                                child: Icon(
-                              Icons.location_on,
-                              color: Colors.blue,
-                            )),
-                            TextSpan(
-                                text: "Longitude:",
-                                style: TextStyle(
-                                    color: primaryColor,
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold)),
-                            TextSpan(
-                                text:
-                                    "\n    ${_currentPosition.longitude.toStringAsFixed(4) ?? "--"}",
-                                style: TextStyle(
-                                  color: textLightColor,
-                                ))
-                          ]),
+                        WidgetSpan(child: Icon(Icons.location_on,color: Colors.blue,)),
+                        TextSpan(
+                          text: "Longitude:",
+                          style: TextStyle(
+                            color: primaryColor,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold
+                          )
                         ),
-                        RichText(
-                          text: TextSpan(children: [
-                            WidgetSpan(
-                                child: Icon(
-                              Icons.location_on,
-                              color: Colors.blue,
-                            )),
-                            TextSpan(
-                                text: "Latitiude:",
-                                style: TextStyle(
-                                    color: primaryColor,
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold)),
-                            TextSpan(
-                                text:
-                                    "\n    ${_currentPosition.latitude.toStringAsFixed(4) ?? "--"}",
-                                style: TextStyle(
-                                  color: textLightColor,
-                                ))
-                          ]),
+                        TextSpan(
+                            text: "\n    ${_currentPosition.longitude.toStringAsFixed(4)?? "--"}",
+                            style: TextStyle(
+                                color: textLightColor,
+                            )
                         )
-                      ],
-                    )
-                  : CircularProgressIndicator(),
-              Container(
-                height: 200,
-                decoration: BoxDecoration(
-                  color: secondaryColor,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    _capturedImage != null
-                        ? Image.file(_capturedImage!)
-                        : IconButton(
-                      icon: Icon(Icons.camera_alt),
-                      onPressed: _openCamera,
-                      iconSize: 50,
-                      color: Colors.white,
+                      ]
                     ),
-                    SizedBox(height: 10),
-                    Text(
-                      Address,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
+                  ),
+                  RichText(
+                    text:TextSpan(
+                        children: [
+                          WidgetSpan(child: Icon(Icons.location_on,color: Colors.blue,)),
+                          TextSpan(
+                              text: "Latitiude:",
+                              style: TextStyle(
+                                  color: primaryColor,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold
+                              )
+                          ),
+                          TextSpan(
+                              text: "\n    ${_currentPosition.latitude.toStringAsFixed(4)??"--"}",
+                              style: TextStyle(
+                                color: textLightColor,
+                              )
+                          )
+                        ]
                     ),
-                  ],
-                ),
-              ),
+                  )
+                ],
+              )
             ],
           ),
         ),
