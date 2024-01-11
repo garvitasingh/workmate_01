d09ed81935ce98bfc6ba87d19b74ff78f24d503e
Rohit Rana <rkrana6631@gmail.com>
2024-01-11 09:32:43 +0530

    login bg

--- lib/view/login_view.dart
+++ lib/view/login_view.dart
@@ -27,58 +27,76 @@ class _LoginViewPageState extends State<LoginViewPage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Image.asset("assets/applogo.png"),
-              Card(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30)),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(children: [
-                    const SizedBox(
-                      height: 40,
+              Image.asset(
+                "assets/applogo.png",
+                color: Colors.white,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "Welcome back",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.w600),
                     ),
-                    Container(
-                      height: 60,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(12),
-                          border: Border.all(color: Colors.grey)),
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                            contentPadding: EdgeInsets.all(12),
-                            border: InputBorder.none,
-                            hintStyle: TextStyle(color: Colors.black),
-                            hintText: "Enter Mobile Number"),
-                      ),
+                  ),
+                  Card(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30)),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(children: [
+                        const SizedBox(
+                          height: 40,
+                        ),
+                        Container(
+                          height: 60,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12),
+                              border: Border.all(color: Colors.grey)),
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                                contentPadding: EdgeInsets.all(12),
+                                border: InputBorder.none,
+                                hintStyle: TextStyle(color: Colors.black),
+                                hintText: "Enter Mobile Number"),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        MaterialButton(
+                          height: 50,
+                          minWidth: MediaQuery.of(context).size.width,
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const HomePageView(),
+                                ));
+                          },
+                          color: const Color.fromARGB(255, 14, 124, 213),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5)),
+                          child: const Text(
+                            "Login",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w600),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 40,
+                        ),
+                      ]),
                     ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    MaterialButton(
-                      height: 50,
-                      minWidth: MediaQuery.of(context).size.width,
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const HomePageView(),
-                            ));
-                      },
-                      color: const Color.fromARGB(255, 14, 124, 213),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(5)),
-                      child: const Text(
-                        "Login",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w600),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 40,
-                    ),
-                  ]),
-                ),
+                  ),
+                ],
               )
             ],
           ),
